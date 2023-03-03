d5c27794979cf5c76fbc92489e08376e0562bbd3
hlkuhn2023 <hlkuhn2023@tamu.edu>
2023-03-03 17:50:26 -0600

    Added git functions

--- dbConnection.js
+++ dbConnection.js
@@ -1,3 +1,9 @@
+/*
+Author:  Harrison Kuhn
+Use:  ECEN 404 Capstone Project
+Team: 59: Motor conveyor for package discribution
+*/
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -6,40 +12,17 @@ const port = 3000;
 var path = require('path');
 
 app.use(cors({origin: "*"}));
-app.use(express.static('serverFiles'));
+app.use(express.static('serverFiles')); //Specifying folder to look for files in
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Connecting to database
-const {Client} = require('pg');
-const {query} = require('express');
+const {Client} = require('pg');     //pg dependency for Postgres
+const {query} = require('express'); //express dependency
 
+//Making client for connecting to database
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL, //Using a set environment variable for connection string
   ssl: {
     rejectUnauthorized: false
   }
@@ -93,3 +76,14 @@ function updateSessionInfo() {
     }
   });
 }
+
+//GET FUNCTIONS
+app.get('/updatePackageInfo', function(req, res) {
+  updatePackageInfo();
+  res.send("Complete");
+});
+
+app.get('/updateSessionInfo', function(req, res) {
+  updateSessionInfo();
+  res.send("Complete");
+});
